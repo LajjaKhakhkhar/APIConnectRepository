fe082ddcba7831ad84533129f1cb6010c2920b79
LajjaKhakhkhar <40431971+LajjaKhakhkhar@users.noreply.github.com>
2018-06-21 12:28:27 +0530

    added wcf

---
+++ WcfServiceLibrary1/ISaleService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace WcfServiceLibrary1
+{
+    [ServiceContract]
+    interface ISaleService
+    {
+        [OperationContract]
+        bool InsertCustomer(Customer obj);
+
+        [OperationContract]
+        List<Customer> GetAllCustomer();
+
+        [OperationContract]
+        bool DeleteCustomer(int Cid);
+
+        [OperationContract]
+        bool UpdateCustomer(Customer obj);
+    }
+
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public int CustomerID;
+        [DataMember]
+        public string CustomerName;
+        [DataMember]
+        public string Address;
+        [DataMember]
+        public string EmailId;
+    } 
+}
